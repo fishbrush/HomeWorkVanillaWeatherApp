f84f8b021c893fd68b0a23eea320a24485dba5cd
fishbrush <93577828+fishbrush@users.noreply.github.com>
2022-06-05 23:22:25 +1200

    added relevant icon from api

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ function formatDate(timestamp) {
   if (hours <10) {
       hours = `0${hours}`
   };
-  
+
   let minutes = date.getMinutes();
   if (minutes < 10) {
     minutes = `0${minutes}`;
@@ -31,15 +31,21 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+      "src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`
+  );
+  iconElement.setAttribute( "alt", response.data.weather[0].description); 
 }
 
 let apiKey = "90bc7e62af1e08f18b00cf6e3cfcd85b";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+let city = "London";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
